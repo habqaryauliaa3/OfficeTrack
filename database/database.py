4281387256546334e55e19730c0577d2eb2f6d04
TigerIsDaBestTanks <tigerbesthehe@gmail.com>
2026-07-02 15:05:04 +0700

    database

--- database/database.py
+++ database/database.py
@@ -1,12 +1,11 @@
+import os
 import sqlite3
 
-DB_PATH = "database/office.db"
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+DB_PATH = os.path.join(BASE_DIR, "database", "office.db")
 
 
 def get_connection():
-
     conn = sqlite3.connect(DB_PATH)
-
     conn.row_factory = sqlite3.Row
-
     return conn
